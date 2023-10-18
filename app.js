962e294d6f38e2cc16573438d3d570145e249fd2
João Pedro Araújo Teixeira <joao.ateixeira2007@gmail.com>
2023-10-18 09:23:36 -0300

    Commit1810232

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ res.render('login');
 app.post('/login', (req, res) => {
 const { username, password } = req.body;
 
-const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+const query = 'SELECT * FROM pacientes WHERE username = ? AND password = ? AND cpf = ?';
 
 db.query(query, [username, password], (err, results) => {
 if (err) throw err;
@@ -62,9 +62,7 @@ res.send('Credenciais incorretas. <a href="/">Tente novamente</a>');
 app.get('/dashboard', (req, res) => {
 
 if (req.session.loggedin) {
-res.send(`Bem-vindo, ${req.session.username}!<br><a href="/logout">Sair</a>`);
-res.sendFile(__dirname + '/views/login.ejs')
-app.use(express.static(__dirname + '/'));
+res.send(`Bem-vindo, ${req.session.username}!<br><a href="/">Sair</a>`);
 } else {
 res.send('Faça login para acessar esta página. <a href="/">Login</a>');
 }
@@ -77,6 +75,51 @@ res.redirect('/');
 });
 });
 
-app.listen(3000, () => {
-console.log('Servidor rodando na porta 3000');
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/img'));
+
+db.connect(err => {
+  if (err) {
+    console.error('Erro na conexão com o banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados estabelecida.');
 });
+//configurar EJS como o motor de visualização
+app.set('view engine', 'ejs');
+
+// Configurar o Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('views'));
+app.get('/',(req, res) => {
+  res.render('cadastro');
+  });
+
+//rota para a pagina de cadastro
+app.get('/cadastro',(req, res) => {
+res.render('cadastro');
+});
+
+app.set('view engine','ejs');
+app.get('/login', (req, res) => {
+res.render('login'); //renders vies/cadastro.ejs
+app.use(express.static(_dirname + '/'));
+});
+// Rota de cadastro
+app.post('/cadastro', (req, res) => {
+  const { username,password } = req.body;
+  const query = 'INSERT INTO pacientes (username, password, cpf) VALUES (?, ? )';
+  db.query(query, [username, password] , (err, result) => {
+    if (err) {
+      console.error('Erro ao cadastrar o usuário:', err);
+      res.status(500).send('Erro ao cadastrar o usuário.');
+    } else {
+      console.log('Usuário cadastrado com sucesso!');
+      res.status(200).send('Usuário cadastrado com sucesso.');
+    }
+  });
+});
+app.listen(3001, () => {
+console.log('Servidor rodando na porta 3001');
+});
+
